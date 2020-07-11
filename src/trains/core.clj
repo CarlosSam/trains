aedbdc65f5fbaa7c3751e455c6932f0cd48d0bf1
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2020-07-11 16:42:51 -0300

    3 cities path

--- src/trains/core.clj
+++ src/trains/core.clj
@@ -8,5 +8,11 @@
 
 ; city-seq in the format of keywords of cities
 ; distances is a map of distances by dest and origin; ex: {:a {:b 2 :c 3}}
+; returns the distance of the whole path or nil if such path is not possible
 (defn calculate-distance [city-seq distances]
-  (get-in distances city-seq))
+  (let [routes (partition 2 1 city-seq)]
+    (try
+      (reduce #(+ %1 (get-in distances %2))
+              0
+              routes)
+      (catch NullPointerException e nil))))
--- test/trains/core_test.clj
+++ test/trains/core_test.clj
@@ -5,4 +5,5 @@
 (deftest a-test
   (testing "calculate-distance"
     (is (= 2 (calculate-distance [:a :b] {:a {:b 2}})))
-    (is (= nil (calculate-distance [:a :c] {:a {:b 2}})))))
+    (is (= nil (calculate-distance [:a :c] {:a {:b 2}})))
+    (is (= 101 (calculate-distance [:a :c :b] {:a {:b 2 :c 1} :b {:a 10 :c 20} :c {:a 200 :b 100}})))))
